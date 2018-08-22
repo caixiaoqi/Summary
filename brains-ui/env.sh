9b80dc5094122d0dc5d554814be71cd6c40fc369
caixiaoqi <1121420679@qq.com>
2018-08-22 13:44:42 +0800

    ready_2

--- brains-ui/env.sh
+++
@@ -1,1 +0,0 @@
-export PATH=/opt/node-v8.11.3-linux-x64/bin:$PATH
--- brains-ui/src/api/dataResource/brainsTree.js
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-import qs from 'qs'
-export function fetchBrainsTree(){
-    return request({
-        url:'/api/v1/catalog/tree',
-        method:'get'
-    })
-}
